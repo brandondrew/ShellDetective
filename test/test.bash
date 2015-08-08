9a52bee00f0483aba71ca4b62bd542233bab4523
Brandon Zylstra <brandon.zylstra@gmail.com>
2015-08-08 15:46:34 -0400

    adding the missing bash test

--- test/test.bash
+++ test/test.bash
@@ -7,9 +7,20 @@ cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
 ### load our testing functions ###
 source "./test_helper"
 
+########################################################################
+### test parent pid ###
   description="Crystal will reliably get the process ID of the shell"
  crystal_ppid=$(./ppid)
      shell_id=$(echo $$)
 
 assert_equal "$crystal_ppid" "$shell_id" "$description"
+
+
+########################################################################
+### test shell_name ###
+description="Crystal will correctly get the name of this file"
+   expected="test.bash"
+     actual=$(../shell_name)
+
+assert_equal "$expected" "$actual" "$description"
 
